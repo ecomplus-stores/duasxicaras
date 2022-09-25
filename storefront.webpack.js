8a60426b575e701a129f8dcf9ca3fcdec4618366
matheusgnreis <matheus@e-com.club>
2022-09-25 18:34:32 -0300

    chore: update blog

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,6 +6,7 @@ module.exports = () => ({
   resolve: {
     alias: {
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html')
+      //'./assets/sitemap.xml.ejs': path.resolve(__dirname, 'template/js/custom-js/html/sitemap.xml.ejs')
     }
   }
 })
