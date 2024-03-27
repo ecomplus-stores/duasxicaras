a04c72112733c0b7889bbd755fa811439108705c
vitorrgg <vitorrggm@gmail.com>
2024-03-27 12:15:45 -0300

    chore(acc_form): altering marketing acceptance text

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -9,6 +9,7 @@ module.exports = () => ({
       './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.js'),
       './js/InstantSearch.js': path.resolve(__dirname, 'template/js/custom-js/html/InstantSearch.js'),
       './js/SearchEngine.js': path.resolve(__dirname, 'template/js/custom-js/html/SearchEngine.js'),
+      './html/AccountForm.html': path.resolve(__dirname, 'template/js/custom-js/components/AccountForm.html'),
       './html/RecommendedItems.html': path.resolve(__dirname, 'template/js/custom-js/html/RecommendedItems.html')
     }
   }
